7c2ed2f1351d6dc1c08f8b0f07cbc412111b4f4b
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2026-05-13 20:32:11 +0300

    Bump patch version

--- osu! overlay/versioning/Version.h
+++ osu! overlay/versioning/Version.h
@@ -59,4 +59,4 @@ public:
     auto operator<=>(const VersionTag&) const = default;
 };
 
-constexpr VersionTag OVERLAY_ENGINE_VERSION = VersionTag(3, 0, 2);
+constexpr VersionTag OVERLAY_ENGINE_VERSION = VersionTag(3, 0, 3);
